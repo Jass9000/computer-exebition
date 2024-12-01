53448854a1c21957cc21126c68e39c72d374157a
Jass9000 <jaiswaldevang.183916@gmail.com>
2024-12-02 00:13:53 +0530

    all done

--- main.py
+++ main.py
@@ -229,9 +229,10 @@ def select(event):
             break
     changebg(index + 1, "y")
     root.update()
+    playsound.playsound("lock.wav")
 
 
-    global i
+
     if value == current_question["answer"]:
         changebg(index + 1, "g")
         playsound.playsound("clap.wav")
@@ -247,7 +248,8 @@ def select(event):
             optionButton3.config(text=next_question["options"][2])
             optionButton4.config(text=next_question["options"][3])
             amountLabel.config(image=amountImages[i])
-            playsound.playsound("next q.wav", block=False)
+            playsound.playsound("next q.wav",block=False)
+            root.after(2000,lambda: say(f'{next_question["question"]} \n option A . {next_question["options"][0]}\n . option B . {next_question["options"][1]} . option C . {next_question["options"][2]} . option D . {next_question["options"][3]} '))
         else:
             display_winnings(i, False)  # If all questions are answered, display final winnings
     else:
@@ -376,7 +378,7 @@ def start():
     optButton=[""]
     # Destroy the start button after clicking
     startbutton.destroy()
-    #playsound.playsound("amitabh.wav")
+    playsound.playsound("amitabh.wav")
     bgimage.destroy()
 
     leftframe = Frame(root, bg='black', padx=90)
@@ -459,8 +461,8 @@ def start():
     optionButton3.bind('<Button-1>', select)
     optionButton4.bind('<Button-1>', select)
     
-    playsound.playsound('next q.wav',block=False)
-    #say(f'{questions_data[i]["question"]} \n option A . {questions_data[i]["options"][0]}\n . option B . {questions_data[i]["options"][1]} . option C . {questions_data[i]["options"][2]} . option D . {questions_data[i]["options"][3]} ')
+    playsound.playsound('next q.wav')
+    say(f'{questions_data[i]["question"]} \n option A . {questions_data[i]["options"][0]}\n . option B . {questions_data[i]["options"][1]} . option C . {questions_data[i]["options"][2]} . option D . {questions_data[i]["options"][3]} ')
 
 # Create the start button
 bgimag = PhotoImage(file='kbc1.png')
